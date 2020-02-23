cdb1e3f69644c87b0e151848e9abe95c7da07b7b
root <nd.ta@hotmail.com>
2020-02-23 06:38:22 +0000

    Added initial robot setup: world and model

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/ball-chaser-robot/catkin_ws/src
+# Build directory: /home/workspace/ball-chaser-robot/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
