bb20f2dae9d65fafd1078491437ec0f202971320
root <nd.ta@hotmail.com>
2020-03-08 19:16:08 +0000

    It's alive!

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,7 +8,13 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-	// TODO
+	ball_chaser::DriveToTarget srv;
+	srv.request.linear_x = lin_x;
+	srv.request.angular_z = ang_z;
+	
+	if (!client.call(srv)) {
+		ROS_ERROR("Falled to call DriveToTarget service");
+	}
 }
 
 // This callback function continuously executes and reads the image data
@@ -35,14 +41,18 @@ void process_image_callback(const sensor_msgs::Image img)
 		}
 	}
 	
-  
-  	ROS_INFO_STREAM("L: "<< left_count << " R: "<<right_count << " M: "<< middle_count);
-	if (left_count > right_count && left_count > middle_count) {
-		ROS_INFO_STREAM("left");
+	if (left_count == 0 && middle_count == 0 && right_count == 0) {
+		ROS_INFO_STREAM("STOP");
+		drive_robot(0.0, 0.0);
+	} else if (left_count > right_count && left_count > middle_count) {
+		ROS_INFO_STREAM("LEFT");
+		drive_robot(0.0, 0.5);
 	} else if (right_count > left_count && right_count > middle_count) {
-		ROS_INFO_STREAM("right");
+		ROS_INFO_STREAM("RIGHT");
+		drive_robot(0.0, -0.5);
 	} else {
-		ROS_INFO_STREAM("forward");
+		ROS_INFO_STREAM("FORWARD");
+		drive_robot(0.5, 0.0);
 	}
 }
 
@@ -53,7 +63,7 @@ int main(int argc, char** argv)
 	ros::NodeHandle n;
 	
 	// Define a client service capable of requesting services from command_robot
-	client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chasser/command_robot");
+	client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
 	
 	// Subscribe to /camera/rgb/image_raw topic to read 
 	// the image data inside the process_image_callback function
